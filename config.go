03b02642b0ce5203a1724cb5f3715de9704b624b
Vladimir Dulenov <vladimir.dulenov@zattoo.com>
2022-06-29 16:23:06 +0200

    Config struct and reader method added

---
+++ config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+// DefultConfigPath is default path for app config from rpm package
+const DefultConfigPath = "/etc/test_quic_cfg.yaml"
+
+// Config structure described yaml structure from /etc/testsrv.yaml
+type Config struct {
+	Urls               []string `yaml:"urls"`
+	MonFile            string   `yaml:"mon_file"`
+	GoroutinesCount    int      `yaml:"goroutines"`
+	ExpectedStatusCode string   `yaml:"expected_status_code"`
+}
+
+// GetConfig reading and parsing configuration yaml file
+func (conf *Config) GetConfig(configPath string) {
+	if configPath == "" {
+		configPath = DefultConfigPath
+	}
+	yamlConfig, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yaml.Unmarshal(yamlConfig, &conf)
+	if err != nil {
+		fmt.Println("Unmarshal config error")
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -19,4 +19,5 @@ require (
 	golang.org/x/tools v0.1.1 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
