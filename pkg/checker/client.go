80464e620c6c62e5385ac7d1849eb9eb0b2ce05d
Vladimir Dulenov <originalrad50@gmail.com>
2023-12-24 16:49:01 +0100

    feat: tests of http client were improved

--- pkg/checker/client.go
+++ pkg/checker/client.go
@@ -28,25 +28,33 @@ func (c *QuicClient) Get(url string, siteStatus chan<- *SiteStatus) {
 
 	res, err := c.httpClient.Do(req)
 	if err != nil {
-		// QoS of UDP is not good enough, especially for small operators, so we need to retry
-		// if it's a Client.Timeout , then try to send one more request
+		// QoS of UDP is not good enough, especially for small network providers,
+		// so we need to retry if it's a Client.Timeout error
 		netErr, isNetErr := err.(net.Error)
 		if isNetErr && netErr.Timeout() {
 			log.Printf("net.Error with a Timeout occured: %v\n", url)
 			res, err := c.httpClient.Do(req)
-			// close response body if request was successful
-			if err == nil {
-				defer res.Body.Close()
+			// if it's still an error, then we need to return it
+			if err != nil {
+				siteStatus <- &SiteStatus{url, -1, err}
+				runtime.Gosched()
+				return
 			}
+			// if it's not an error, then we need to return the response status code
+			// close response body
+			defer res.Body.Close()
 			siteStatus <- &SiteStatus{url, res.StatusCode, err}
 			runtime.Gosched()
 			return
 		}
+		// if it's not a timeout error, then we need to return this errorx
+		siteStatus <- &SiteStatus{url, -1, err}
+		runtime.Gosched()
+		return
+
 	}
 	// close response body
-	if err == nil {
-		defer res.Body.Close()
-	}
+	defer res.Body.Close()
 
 	siteStatus <- &SiteStatus{url, res.StatusCode, err}
 	runtime.Gosched()
