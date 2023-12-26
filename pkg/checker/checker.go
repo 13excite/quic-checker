a562ea8a09317ddc93427191a507df9f6710da47
Vladimir Dulenov <originalrad50@gmail.com>
2023-12-26 21:58:18 +0100

    feat: simple worker func was added

--- pkg/checker/checker.go
+++ pkg/checker/checker.go
@@ -1,7 +1,21 @@
 package checker
 
+import (
+	"log"
+)
+
 type SiteStatus struct {
 	URL        string
 	StatusCode int
 	Err        error
+}
+
+// TODO: add cli and prometheus output modes
+func siteStatusChecker(results <-chan *SiteStatus) {
+	for result := range results {
+		if result.Err != nil {
+			log.Print("HTTP/3 check error on url: ", result.URL, result.Err)
+			continue
+		}
+	}
 }
